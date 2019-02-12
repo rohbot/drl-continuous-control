b49d74978831ef213e27586c65c7fe5eb991b21b
Rohan Ahmad <rohanoahmad@gmail.com>
2019-02-12 20:21:43 +0700

    added train.py

--- train.py
+++ train.py
@@ -1,75 +1,279 @@
-from comet_ml import Experiment
+import sys
+import random
+from collections import namedtuple, deque
+
+import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import numpy as np
 
 from unityagents import UnityEnvironment
-import numpy as np
-import random
-import torch
-from collections import deque
-import matplotlib.pyplot as plt
-from ddpg_agent import Agent
+
+#from model import PPOPolicyNetwork
+#from agent import PPOAgent
 import os
 import pickle
 import time
 
+
 mqtt_cmd = "mosquitto_pub -h 10.0.0.1 -t drl/"
-
-#env = UnityEnvironment(file_name='20_agents/Reacher_Linux_NoVis/Reacher.x86_64')
-env = UnityEnvironment(file_name='one_agent/Reacher_Linux_NoVis/Reacher.x86_64')
-
+env = UnityEnvironment(file_name='./Reacher_Linux_NoVis/Reacher.x86_64')
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
-
-# reset the environment
 env_info = env.reset(train_mode=True)[brain_name]
-# number of agents
-num_agents = len(env_info.agents)
-print('Number of agents:', num_agents)
-states = env_info.vector_observations                  # get the current state (for each agent)
-agent = Agent(state_size=states.shape[1], action_size=brain.vector_action_space_size, random_seed=10)
 
-experiment = Experiment(project_name="reacher")
+config = {
+    'environment': {
+        'state_size':  env_info.vector_observations.shape[1],
+        'action_size': brain.vector_action_space_size,
+        'number_of_agents': len(env_info.agents)
+    },
+    'pytorch': {
+        'device': torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    },
+    'hyperparameters': {
+        'discount_rate': 0.99,
+        'tau': 0.95,
+        'gradient_clip': 5,
+        'rollout_length': 2048,
+        'optimization_epochs': 10,
+        'ppo_clip': 0.2,
+        'log_interval': 2048,
+        'max_steps': 1e5,
+        'mini_batch_number': 32,
+        'entropy_coefficent': 0.01,
+        'episode_count': 300,
+        'hidden_size': 512,
+        'adam_learning_rate': 3e-4,
+        'adam_epsilon': 1e-5
+    }
+}
 
-def ddpg(n_episodes=2000, max_t=70):
-    scores_deque = deque(maxlen=100)
-    scores_global = []
-    max_score = -np.Inf
-    for i_episode in range(1, n_episodes+1):
-        env_info = env.reset(train_mode=True)[brain_name]     # reset the environment    
-        states = env_info.vector_observations                  # get the current state (for each agent)
-        #print(states)
-        scores = np.zeros(num_agents)                          # initialize the score (for each agent)
-        agent.reset()
-        scores_average = 0
-        #score = 0
-        for t in range(max_t):
-            actions = agent.act(states)
-            env_info = env.step(actions)[brain_name]           # send all actions to tne environment
-            next_states = env_info.vector_observations         # get next state (for each agent)
-            rewards = env_info.rewards                         # get reward (for each agent)
-            dones = env_info.local_done                        # see if episode finished
-            agent.step(states, actions, rewards, next_states, dones)
-            states = next_states                               # roll over states to next time step
-            scores += rewards                                  # update the score (for each agent)            
-            if np.any(dones):                                  # exit loop if episode finished
-                break       
-        score = np.mean(scores)
-        scores_deque.append(score)
-        scores_global.append(score)
-        score_average = np.mean(scores_deque)
+class Batcher:
+    
+    def __init__(self, batch_size, data):
+        self.batch_size = batch_size
+        self.data = data
+        self.num_entries = len(data[0])
+        self.reset()
+
+    def reset(self):
+        self.batch_start = 0
+        self.batch_end = self.batch_start + self.batch_size
+
+    def end(self):
+        return self.batch_start >= self.num_entries
+
+    def next_batch(self):
+        batch = []
+        for d in self.data:
+            batch.append(d[self.batch_start: self.batch_end])
+        self.batch_start = self.batch_end
+        self.batch_end = min(self.batch_start + self.batch_size, self.num_entries)
+        return batch
+
+    def shuffle(self):
+        indices = np.arange(self.num_entries)
+        np.random.shuffle(indices)
+        self.data = [d[indices] for d in self.data]
+
+
+class PPOAgent(object):
+    
+    def __init__(self, environment, brain_name, policy_network, optimizier, config):
+        self.config = config
+        self.hyperparameters = config['hyperparameters']
+        self.network = policy_network
+        self.optimizier = optimizier
+        self.total_steps = 0
+        self.all_rewards = np.zeros(config['environment']['number_of_agents'])
+        self.episode_rewards = []
+        self.environment = environment
+        self.brain_name = brain_name
         
-        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
-        if i_episode % 100 == 0:
-            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
-            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
-		
-        os.system(mqtt_cmd +  "episode -m " + str(i_episode))
-        os.system(mqtt_cmd + "score -m {:.2f}".format(score))
-        os.system(mqtt_cmd + "average -m {:.2f}".format(np.mean(score_average)))
-        experiment.log_current_epoch(i_episode)
-        experiment.log_metric("score", score, step=i_episode)
-        experiment.log_metric("score_average", score_average, step=i_episode)
+        env_info = environment.reset(train_mode=True)[brain_name]    
+        self.states = env_info.vector_observations              
 
-    return scores_global
+    def step(self):
+        rollout = []
+        hyperparameters = self.hyperparameters
 
-scores = ddpg()
+        env_info = self.environment.reset(train_mode=True)[self.brain_name]    
+        self.states = env_info.vector_observations  
+        states = self.states
+        for _ in range(hyperparameters['rollout_length']):
+            actions, log_probs, _, values = self.network(states)
+            env_info = self.environment.step(actions.cpu().detach().numpy())[self.brain_name]
+            next_states = env_info.vector_observations
+            rewards = env_info.rewards
+            terminals = np.array([1 if t else 0 for t in env_info.local_done])
+            self.all_rewards += rewards
+            
+            for i, terminal in enumerate(terminals):
+                if terminals[i]:
+                    self.episode_rewards.append(self.all_rewards[i])
+                    self.all_rewards[i] = 0
+                    
+            rollout.append([states, values.detach(), actions.detach(), log_probs.detach(), rewards, 1 - terminals])
+            states = next_states
+
+        self.states = states
+        pending_value = self.network(states)[-1]
+        rollout.append([states, pending_value, None, None, None, None])
+
+        processed_rollout = [None] * (len(rollout) - 1)
+        advantages = torch.Tensor(np.zeros((self.config['environment']['number_of_agents'], 1)))
+        returns = pending_value.detach()
+        for i in reversed(range(len(rollout) - 1)):
+            states, value, actions, log_probs, rewards, terminals = rollout[i]
+            terminals = torch.Tensor(terminals).unsqueeze(1)
+            rewards = torch.Tensor(rewards).unsqueeze(1)
+            actions = torch.Tensor(actions)
+            states = torch.Tensor(states)
+            next_value = rollout[i + 1][1]
+            returns = rewards + hyperparameters['discount_rate'] * terminals * returns
+
+            td_error = rewards + hyperparameters['discount_rate'] * terminals * next_value.detach() - value.detach()
+            advantages = advantages * hyperparameters['tau'] * hyperparameters['discount_rate'] * terminals + td_error
+            processed_rollout[i] = [states, actions, log_probs, returns, advantages]
+
+        states, actions, log_probs_old, returns, advantages = map(lambda x: torch.cat(x, dim=0), zip(*processed_rollout))
+        advantages = (advantages - advantages.mean()) / advantages.std()
+
+        batcher = Batcher(states.size(0) // hyperparameters['mini_batch_number'], [np.arange(states.size(0))])
+        for _ in range(hyperparameters['optimization_epochs']):
+            batcher.shuffle()
+            while not batcher.end():
+                batch_indices = batcher.next_batch()[0]
+                batch_indices = torch.Tensor(batch_indices).long()
+                sampled_states = states[batch_indices]
+                sampled_actions = actions[batch_indices]
+                sampled_log_probs_old = log_probs_old[batch_indices]
+                sampled_returns = returns[batch_indices]
+                sampled_advantages = advantages[batch_indices]
+
+                _, log_probs, entropy_loss, values = self.network(sampled_states, sampled_actions)
+                ratio = (log_probs - sampled_log_probs_old).exp()
+                obj = ratio * sampled_advantages
+                obj_clipped = ratio.clamp(1.0 - hyperparameters['ppo_clip'],
+                                          1.0 + hyperparameters['ppo_clip']) * sampled_advantages
+                policy_loss = -torch.min(obj, obj_clipped).mean(0) - hyperparameters['entropy_coefficent'] * entropy_loss.mean()
+
+                value_loss = 0.5 * (sampled_returns - values).pow(2).mean()
+
+                self.optimizier.zero_grad()
+                (policy_loss + value_loss).backward()
+                nn.utils.clip_grad_norm_(self.network.parameters(), hyperparameters['gradient_clip'])
+                self.optimizier.step()
+
+        steps = hyperparameters['rollout_length'] * self.config['environment']['number_of_agents']
+        self.total_steps += steps
+
+
+class FullyConnectedNetwork(nn.Module):
+    
+    def __init__(self, state_size, output_size, hidden_size, output_gate=None):
+        super(FullyConnectedNetwork, self).__init__()
+        self.linear1 = nn.Linear(state_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, output_size)
+        self.output_gate = output_gate
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        if self.output_gate:
+            x = self.output_gate(x)
+        return x
+
+
+class PPOPolicyNetwork(nn.Module):
+    
+    def __init__(self, config):
+        super(PPOPolicyNetwork, self).__init__()
+        state_size = config['environment']['state_size']
+        action_size = config['environment']['action_size']
+        hidden_size = config['hyperparameters']['hidden_size']
+        device = config['pytorch']['device']
+
+        self.actor_body = FullyConnectedNetwork(state_size, action_size, hidden_size, F.tanh)
+        self.critic_body = FullyConnectedNetwork(state_size, 1, hidden_size)  
+        self.std = nn.Parameter(torch.ones(1, action_size))
+        self.to(device)
+
+    def forward(self, obs, action=None):
+        obs = torch.Tensor(obs)
+        a = self.actor_body(obs)
+        v = self.critic_body(obs)
+        
+        dist = torch.distributions.Normal(a, self.std)
+        if action is None:
+            action = dist.sample()
+        log_prob = dist.log_prob(action)
+        log_prob = torch.sum(log_prob, dim=1, keepdim=True)
+        return action, log_prob, torch.Tensor(np.zeros((log_prob.size(0), 1))), v
+
+
+def play_round(env, brain_name, policy, config):
+    env_info = env.reset(train_mode=True)[brain_name]    
+    states = env_info.vector_observations                 
+    scores = np.zeros(config['environment']['number_of_agents'])                         
+    while True:
+        actions, _, _, _ = policy(states)
+        env_info = env.step(actions.cpu().detach().numpy())[brain_name]
+        next_states = env_info.vector_observations         
+        rewards = env_info.rewards                         
+        dones = env_info.local_done                     
+        scores += env_info.rewards                      
+        states = next_states                               
+        if np.any(dones):                                  
+            break
+    
+    return np.mean(scores)
+    
+def ppo(env, brain_name, policy, config, train):
+    if train:
+        optimizier = optim.Adam(policy.parameters(), config['hyperparameters']['adam_learning_rate'], 
+                        eps=config['hyperparameters']['adam_epsilon'])
+        agent = PPOAgent(env, brain_name, policy, optimizier, config)
+        all_scores = []
+        averages = []
+        last_max = 30.0
+        
+        for i in tqdm.tqdm(range(config['hyperparameters']['episode_count'])):
+            agent.step()
+            last_mean_reward = play_round(env, brain_name, policy, config)
+            all_scores.append(last_mean_reward)
+            last_average = np.mean(np.array(all_scores[-100:])) if len(all_scores) > 100 else np.mean(np.array(all_scores))
+            averages.append(last_average)
+            
+            if last_average > last_max:
+                torch.save(policy.state_dict(), f"models/ppo-max-hiddensize-{config['hyperparameters']['hidden_size']}.pth")
+                last_max = last_average
+            print('Episode: {} Total score this episode: {:.2f} Last {:.2f} average: {:.2f}'.format(i + 1, last_mean_reward, min(i + 1, 100), last_average))
+            os.system(mqtt_cmd +  "episode -m " + str(i + 1))
+            #print(last_average)
+            os.system(mqtt_cmd + "score -m {:.2f}".format(last_mean_reward))
+            os.system(mqtt_cmd + "average -m {:.2f}".format(np.mean(last_average)))
+            if last_average > 30:
+            	msg = '"Environment solved in {:d} episodes!\tAverage Score: {:.2f}"'.format(i-100, np.mean(last_average))
+            	os.system(mqtt_cmd + "done -m " + msg)
+            	print(msg)
+            	return all_scores, averages
+        
+        return all_scores, averages
+    else:
+        score = play_round(env, brain_name, policy, config)
+        return [score], [score]
+
+new_policy = PPOPolicyNetwork(config)
+all_scores, average_scores = ppo(env, brain_name, new_policy, config, train=True)
+timestamp =  str(int(time.time()))
+pickle.dump( all_scores, open( "all_scores_"+timestamp+".p", "wb" ) )
+pickle.dump( average_scores, open( "avg_scores_"+timestamp+".p", "wb" ) )
